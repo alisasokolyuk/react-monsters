9412db81dccd66ebe1ad102fa885016b1b303cb9
Alisa Sokolyuk <alisa.sokolyuk@osf.digital>
2021-07-27 11:59:37 +0300

    completed react app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Component } from 'react';
 import {CardList} from './components/card-list/card-list.compoent.jsx';
+import {SearchBox} from './components/search-box/search-box.component.jsx';
 import './App.css';
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     }
+
   }
 
   componentDidMount() {
@@ -16,15 +19,21 @@ class App extends Component {
     .then(users => this.setState({monsters: users}));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
+    const { monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLocaleLowerCase()));
     return (
       <div className="App">
-        <CardList name='Alisa'>
-        {
-          this.state.monsters.map(monster => (
-          <h1 key={monster.id}> {monster.name} </h1>))
-        }
-        </CardList>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+          placeholder="search monsters"
+          handleChange={this.handleChange }/>
+        <CardList monsters={filteredMonsters}/>
       </div>
     );
   }
